17028f17d26ee8c3f3930f2add8dd048f6b7231a
fabiocaettano <fabio.caettano74@gmail.com>
2025-03-16 19:35:00 -0300

    feat: implementado o POST

---
+++ fc-clean-architecture/src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import CreateProductUsecase from "../../../usecase/product/create/create.product.usecase";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+
+export const productRoute = express.Router();
+
+productRoute.post("/", async(req: Request, res: Response) => {
+
+    const usecase = new CreateProductUsecase(new ProductRepository());
+
+    try{
+        const productDto = {
+            type: req.body.type,
+            name : req.body.name,
+            price: req.body.price
+        }
+
+        const output = await usecase.execute(productDto);
+
+        res.send(output);
+
+    }catch(err){
+        res.status(500).send(err);
+    }
+
+})
