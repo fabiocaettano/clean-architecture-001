12e9637f4786ef60a12319c395c26b27b8475f4d
fabiocaettano <fabio.caettano74@gmail.com>
2025-04-05 13:18:16 -0300

    feat: implementado verbo put e configurado o presenters para product

--- fc-clean-architecture/src/infrastructure/api/routes/product.route.ts
+++ fc-clean-architecture/src/infrastructure/api/routes/product.route.ts
@@ -3,8 +3,27 @@ import CreateProductUsecase from "../../../usecase/product/create/create.product
 import ProductRepository from "../../product/repository/sequelize/product.repository";
 import ListProductUsecase from "../../../usecase/product/list/list.product.usecase";
 import FindProductUseCase from "../../../usecase/product/find/find.product.usecase";
+import ProductPresenter from "../presenters/product.presenter";
 
 export const productRoute = express.Router();
+
+productRoute.put("/:id", async(req: Request, res: Response) => {
+    const usecase = new CreateProductUsecase(new ProductRepository());
+    try{    
+        const productDto = {
+            id: req.params.id,
+            type: req.body.type,
+            name : req.body.name,
+            price: req.body.price
+        }
+        const output = await usecase.execute(productDto);
+        res.send(output);
+    }catch(err){    
+        res.status(500).send(err);
+    }
+});
+        
+
 
 productRoute.post("/", async(req: Request, res: Response) => {
     const usecase = new CreateProductUsecase(new ProductRepository());
@@ -25,7 +44,10 @@ productRoute.get("/", async(req: Request, res: Response) => {
     const usecase = new ListProductUsecase(new ProductRepository());
     try{
         const output = await usecase.execute();
-        res.send(output);
+        res.format({                
+            json: async () => res.send(output),                
+            xml: async () => res.send(ProductPresenter.listXML(output)),
+        });  
     }catch(err){
         res.status(500).send(err);
     }
@@ -34,8 +56,11 @@ productRoute.get("/", async(req: Request, res: Response) => {
 productRoute.get("/:id", async(req: Request, res: Response) => {
     const usecase = new FindProductUseCase(new ProductRepository());
     try{
-        const output = await usecase.execute({id: req.params.id});
-        res.send(output);
+        const output = await usecase.execute({id: req.params.id});        
+        res.format({    
+            json: async () => res.send(output),    
+            xml: async () => res.send(ProductPresenter.XML(output)),
+        });  
     }catch(err){
         res.status(500).send(err);
     }
